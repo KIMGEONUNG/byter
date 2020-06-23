9b1e3dcee6913331c2ac61c42f4ba1ad436fdfcd
KIMGEONUNG <saywooong@gmail.com>
2020-06-24 07:58:02 +0900

    chore: add test project

---
+++ byterTest/BOMDetectorTest.cs
@@ -0,0 +1,23 @@
+﻿using byter.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace byterTest
+{
+    [TestClass]
+    public class BOMDetectorTest
+    {
+        [TestMethod]
+        public void DetectBOM()
+        {
+            BOMDetector bd;
+            byte[] bts;
+
+            bts = new byte[]{ 0xFF, 0xFE, 0x00, 0x00,  };
+            bd = new BOMDetector();
+
+        }
+    }
+}
---
+++ byterTest/ByteToNumberTest.cs
@@ -0,0 +1,75 @@
+﻿using byter.Enums;
+using byter.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace byterTest
+{
+    [TestClass]
+    public class ByteToNumberTest
+    {
+        [TestMethod]
+        public void ToDouble()
+        {
+            IEnumerable<byte> binaries = null;
+            Endian endian;
+            double[] answer = null;
+            double[] result = null;
+
+            // Big endian test 
+            answer = new double[] { 1 };
+            binaries = new byte[] 
+            {  0b00111111, 0b11110000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, };
+            endian = Endian.Big;
+            result = ByteToNumber.ToDouble(binaries, endian);
+
+            Assert.IsTrue(answer.SequenceEqual(result));
+
+            // Little endian test 
+            answer = new double[] { 23 };
+            binaries = new byte[] 
+            { 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00110111, 0b01000000 };
+            endian = Endian.Little;
+            result = ByteToNumber.ToDouble(binaries, endian);
+
+            Assert.IsTrue(answer.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void ToInt()
+        {
+            IEnumerable<byte> binaries = null;
+            Endian endian;
+            int[] answer = null; 
+            int[] result = null;
+
+            // Big endian test 
+            answer = new int[] { 512 };
+            binaries = new byte[] { 0b00000000, 0b00000000, 0b00000010, 0b00000000, };
+            endian = Endian.Big;
+            result = ByteToNumber.ToInt(binaries, endian);
+
+            Assert.IsTrue(answer.SequenceEqual(result));
+
+            // Big endian test  
+            answer = new int[] { 1000 };
+            binaries = new byte[] { 0b00000000, 0b00000000, 0b00000011, 0b11101000, };
+            endian = Endian.Big;
+            result = ByteToNumber.ToInt(binaries, endian);
+
+            Assert.IsTrue(answer.SequenceEqual(result));
+
+            // Little endian test 
+            answer = new int[] { 1000 };
+            binaries = new byte[] { 0b11101000,  0b00000011, 0b00000000, 0b00000000, };
+            endian = Endian.Little;
+            result = ByteToNumber.ToInt(binaries, endian);
+
+            Assert.IsTrue(answer.SequenceEqual(result));
+
+        }
+    }
+}
